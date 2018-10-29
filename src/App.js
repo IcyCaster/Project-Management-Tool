245807685cec969a60cae52db03805282ba2b1cd
Chester Booker <chester.booker@myob.com>
2018-10-30 09:53:40 +1300

    React boiler code

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import './app.css';
+
+export default class App extends Component {
+    render() {
+        return (
+            <div>
+                <h1>My React App!</h1>
+            </div>
+        );
+    }
+}
